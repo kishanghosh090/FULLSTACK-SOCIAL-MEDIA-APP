23bff73cf2181d972545b1290372ecadfe53b319
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-11 22:43:47 +0530

    file added

--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getMessageSuccess, getMessageError } from "../../Hooks/popUpMessage";
 import { Toaster } from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
-import {baseUrl} from "../../../baseUrl.js";
+import { baseUrl } from "../../../baseUrl.js";
 function Register() {
   const navigate = useNavigate();
   const [data, setData] = useState({
